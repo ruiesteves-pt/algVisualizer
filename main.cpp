ceb7ff2e6e2b1012c3510d067db14b3918486311
ruiesteves-pt <ruiesteves97@gmail.com>
2024-07-21 22:17:36 +0200

    heap sort added

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@
 #include <chrono>
 #include <thread>
 
+enum SortAlgorithm { BUBBLE_SORT, HEAP_SORT };
+
+// Utils
 void visualizeArray(sf::RenderWindow &window, const std::vector<int> &array) {
     window.clear();
     for (size_t i = 0; i < array.size(); ++i) {
@@ -15,6 +18,7 @@ void visualizeArray(sf::RenderWindow &window, const std::vector<int> &array) {
     window.display();
 }
 
+// Sorting Algorithms
 void bubbleSort(sf::RenderWindow &window, std::vector<int> &array) {
     for (size_t i = 0; i < array.size() - 1; ++i) {
         for (size_t j = 0; j < array.size() - i - 1; ++j) {
@@ -27,6 +31,43 @@ void bubbleSort(sf::RenderWindow &window, std::vector<int> &array) {
     }
 }
 
+void heapify(std::vector<int> &array, int n, int i, sf::RenderWindow &window) {
+    int largest = i;
+    int left = 2*i + 1;
+    int right = 2*i + 2;
+
+    if (left < n && array[left] > array[largest]) {
+        largest = left;
+    }
+
+    if (right < n && array[right] > array[largest]) {
+        largest = right;
+    }
+
+    if (largest != i) {
+        std::swap(array[i], array[largest]);
+        visualizeArray(window, array);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        heapify(array, n, largest, window);
+    }
+}
+
+
+void heapSort(sf::RenderWindow &window, std::vector<int> &array) {
+    int n = array.size();
+
+    for (int i = n / 2 -1; i >=0; i--) {
+        heapify(array, n, i, window);
+    }
+
+    for (int i = n - 1; i > 0; i--){
+        std::swap(array[0], array[i]);
+        visualizeArray(window, array);
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        heapify(array, i, 0, window);
+    }
+}
+
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800,600), "Algorithm Visualizer");
@@ -34,18 +75,62 @@ int main() {
     std::vector<int> array(50);
     std::generate(array.begin(), array.end(), []() {return rand() % 600; });
 
+    SortAlgorithm currentAlgorithm = BUBBLE_SORT;
+    bool isSorting = false;
+
+    // Load font
+    sf::Font font;
+    if (!font.loadFromFile("arial.ttf")) {
+       return -1;
+    }
+
+    sf::Text text;
+    text.setFont(font);
+    text.setCharacterSize(24);
+    text.setFillColor(sf::Color::White);
+    text.setPosition(10,10);
+    text.setString("Current Algorithm: Bubble Sort");
+
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Num1) {
+                    currentAlgorithm = BUBBLE_SORT;
+                    text.setString("Current Algorithm: Bubble Sort");
+                    std::generate(array.begin(), array.end(), []() { return rand() % 600;});
+                    isSorting = false;
+                }
+
+                if (event.key.code == sf::Keyboard::Num2) {
+                    currentAlgorithm = HEAP_SORT;
+                    text.setString("Current Algorithm: Heap Sort");
+                    std::generate(array.begin(), array.end(), []() { return rand() % 600;});
+                    isSorting = false;
+                }
+
+                if (event.key.code == sf::Keyboard::Enter) {
+                    isSorting = true;
+                }
+            }
+            
         }
 
-        bubbleSort(window, array);
+        if (isSorting) {
+            if (currentAlgorithm == BUBBLE_SORT) {
+                bubbleSort(window, array);
+            } else if (currentAlgorithm == HEAP_SORT) {
+                heapSort(window, array);
+            }
+            isSorting = false;
+        }
 
         window.clear();
         visualizeArray(window, array);
+        window.draw(text);
         window.display();
     }
 
